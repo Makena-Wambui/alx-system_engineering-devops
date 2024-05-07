d45cf800293cf88d5714fe608d13729cfb88f3d2
Makena-Wambui <iammakena@gmail.com>
2024-05-07 08:10:14 -0700

    Puppet for configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Puppet manifest for seting  up your client SSH configuration file so that you can connect
+# to a server without typing a password.
+
+file_line { 'password_auntheticate':
+    path => '/etc/ssh/ssh_config',
+    line => 'PasswordAuthentication no', # ensures line is in file specified in path
+}
+
+file_line { 'identity_file':
+    path => '/etc/ssh/ssh_config',
+    line => 'IdentityFile ~/.ssh/school',
+}
