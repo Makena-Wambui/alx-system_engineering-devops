921241ac11fb496761fbe884128aec7bd46a592a
Makena-Wambui <iammakena@gmail.com>
2024-06-29 15:53:47 +0300

    Using Puppet to configure the ssh client global configuration file.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 # to a server without typing a password.
 
 file_line { 'password_auntheticate':
-    path => '~/.ssh/config',
+    path => '/etc/ssh/ssh_config',
     line => 'PasswordAuthentication no', # ensures line is in file specified in path
 }
 
 file_line { 'identity_file':
-    path => '~/.ssh/config',
+    path => '/etc/ssh/ssh_config',
     line => 'IdentityFile ~/.ssh/school',
 }
