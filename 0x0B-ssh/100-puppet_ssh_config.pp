a4501ce024dccc994197db9ff6393ee7a95124bc
Makena-Wambui <iammakena@gmail.com>
2024-07-01 07:08:50 +0300

    Installing the stdlibmodule

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,8 @@
 # Puppet manifest for seting  up your client SSH configuration file so that you can connect
 # to a server without typing a password.
 
-file_line { 'password_auntheticate':
+
+  file_line { 'password_auntheticate':
     path => '/etc/ssh/ssh_config',
     line => 'PasswordAuthentication no', # ensures line is in file specified in path
 }
@@ -10,3 +11,4 @@ file_line { 'identity_file':
     path => '/etc/ssh/ssh_config',
     line => 'IdentityFile ~/.ssh/school',
 }
+
