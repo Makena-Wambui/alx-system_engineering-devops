9c05a69879e54936c00f9eec3ec28233281f0d6d
Makena-Wambui <iammakena@gmail.com>
2024-07-08 14:06:20 +0300

    Automation: Doing task 1 with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,10 @@
-# Ensure the system is up to date
-exec { 'update_upgrade':
-  command  => 'sudo apt-get update -y && sudo apt-get upgrade -y',
+#!/usr/bin/env bash
+# task advance with pupppet
+
+exec { 'add_http_header_with_puppet':
+  command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
   provider => shell,
-  path     => '/usr/bin:/bin:/usr/sbin:/sbin',
-}
-
-# Install Nginx package
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['update_upgrade'],
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => Package['nginx'],
-}
-
-# Add the X-Served-By header to the Nginx configuration
-file_line { 'add_x_served_by_header':
-  path    => '/etc/nginx/nginx.conf',
-  line    => "add_header X-Served-By ${facts['networking']['hostname']};",
-  match   => 'sendfile on;',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Restart Nginx to apply the configuration change
-exec { 'restart_nginx':
-  command     => '/usr/bin/sudo service nginx restart',
-  refreshonly => true,
-  subscribe   => File_line['add_x_served_by_header'],
 }
