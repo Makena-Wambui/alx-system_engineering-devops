931ee676b99e5fba8967916334b2a9bbbe6172cd
Makena-Wambui <iammakena@gmail.com>
2024-07-29 23:06:39 +0300

    Exporting data in the JSON format.

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+export data from the api in the JSON format.
+
+Records all tasks from all employees:
+    Format must be:
+    { "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS},
+    {"username": "USERNAME", "task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS}, ... ],
+    "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS},
+    {"username": "USERNAME", "task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS}, ... ]}
+
+File name must be: todo_all_employees.json
+"""
+
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
