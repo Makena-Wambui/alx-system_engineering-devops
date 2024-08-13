e3349f37c68842eb8fd2aabf3d6794db52827dde
Makena-Wambui <iammakena@gmail.com>
2024-08-13 12:34:47 +0300

    Number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -26,7 +26,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers)
 
         # if request was unsuccessful:
         if response.status_code != 200:
