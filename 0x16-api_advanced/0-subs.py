540832b99184aa838fce5195afb90f4878547d5b
Makena-Wambui <iammakena@gmail.com>
2024-08-13 19:20:27 +0300

    Used auth token

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,7 @@ If you’re getting errors related to Too Many Requests,
 ensure you’re setting a custom User-Agent.
 """
 
+import json
 import requests
 
 
@@ -20,6 +21,25 @@ def number_of_subscribers(subreddit):
     If invalid subreddit, return 0.
 
     """
+    USERNAME = 'Forward-Age6992'
+    CLIENT_ID = "5BXUX6gOYQJ8GzmvXZ6uUw"
+    SECRET = "Ic6OkjkdCEJ52Wp4DQuxUluKczP2Bg"
+    PASSWORD = '1010ilovelucibel'
+
+    # Request an OAuth Token from Reddit
+    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET)
+
+    # with open('passwd.txt', 'r') as f:
+    # pw = f.read().strip()
+
+    data = {
+        'grant_type': 'password',
+        'username': USERNAME,
+        'password': PASSWORD
+    }
+
+    headers = {'User-Agent': 'MyRedditApp/0.1 by Forward-Age6992'}
+
     # url = 'https://www.reddit.com/r/gaming/about'
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'MyRedditApp/0.1 by Forward-Age6992'}
@@ -27,7 +47,8 @@ def number_of_subscribers(subreddit):
     if subreddit:
         try:
             response = requests.get(url,
-                                    headers=headers, allow_redirects=False)
+                                    headers=headers, auth=auth,
+                                    data=data, allow_redirects=False)
 
             response.raise_for_status()
 
