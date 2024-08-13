d01f63df6cf709d64770be4ff4fecf2dfb5540f5
Makena-Wambui <iammakena@gmail.com>
2024-08-13 13:17:02 +0300

    Number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,47 @@
 #!/usr/bin/python3
-""" a function that queries the Reddit API and returns
-the number of subscribers (not active users, total subscribers)
-for a given subreddit.
+
+"""
+A function that queries the Reddit API,
+and returns the number of total subscribers for a given subreddit.
 If an invalid subreddit is given, the function should return 0.
+
+No authentication is necessary for most features of the Reddit API.
+If you’re getting errors related to Too Many Requests,
+ensure you’re setting a custom User-Agent.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    # Construct the URL for the subreddit's about page in JSON format
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers={'User-Agent': 'app/1.0'})
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    """
+    Returns the total number of subscribers to a particular subreddit.
+
+    If invalid subreddit, return 0.
+
+    """
+    # url = 'https://www.reddit.com/r/gaming/about'
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'MyRedditApp/0.1 by Forward-Age6992'}
+
+    if subreddit:
+        try:
+            response = requests.get(url, headers=headers, allow_redirects=False)
+
+            response.raise_for_status()
+
+            response = response.json()
+
+            data = response.get('data')
+            if data is None:
+                return 0
+
+            # if subscribers key does not exist, return 0
+            subs = data.get('subscribers', 0)
+
+            return subs
+
+        except Exception:
+            return 0
     else:
         return 0
